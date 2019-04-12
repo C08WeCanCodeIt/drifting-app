2f15e168f3a4c6757cfb1ba804d990db09cac086
chiuy5 <chiuy5@uw.edu>
2019-04-12 11:26:50 -0700

    made changes

---
+++ drifting-app/build/precache-manifest.86774740795fb44f316405cc44e2c8df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b086b9e878259fde17a8",
+    "url": "/drifting-app/static/css/main.51ce3962.chunk.css"
+  },
+  {
+    "revision": "b086b9e878259fde17a8",
+    "url": "/drifting-app/static/js/main.d22f23e5.chunk.js"
+  },
+  {
+    "revision": "6e22ed990bd6eca3d076",
+    "url": "/drifting-app/static/js/runtime~main.32e38ed2.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/drifting-app/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/drifting-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "dd4ffb98d08e4ebc65ee7768d76cc8f6",
+    "url": "/drifting-app/index.html"
+  }
+];
